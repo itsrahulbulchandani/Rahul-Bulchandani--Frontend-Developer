c7f7d9875cd365123e2cc1fc0ec07bcacc7e9679
itsrahulbulchandani <73634522+itsrahulbulchandani@users.noreply.github.com>
2024-02-19 03:34:14 +0530

    removing console.log

--- src/components/Home.js
+++ src/components/Home.js
@@ -23,7 +23,6 @@ export const Home = () => {
       const response = await fetch(
         `https://www.themealdb.com/api/json/v1/1/filter.php?a=${selectedLocation}`
       );
-      console.log(response);
       const foodItemsObj = await response.json();
       setFoodItems(foodItemsObj);
     } catch (error) {
